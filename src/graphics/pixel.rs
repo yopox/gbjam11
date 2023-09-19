6e99d3efd449fd93c36417a32c2a6ce2d613fe5d
Clement de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2023-09-19 22:45:39 +0200

    pixel: Add scale to FakeTransform

--- src/graphics/pixel.rs
+++ src/graphics/pixel.rs
@@ -1,15 +1,20 @@
-use bevy::prelude::{Component, Query, Transform, Vec3};
+use bevy::prelude::{Component, Query, Transform, Vec2, Vec3};
 
 /// Manipulate this component instead of [Transform] to ensure that
 /// sprites are never drawn on subpixels.
 #[derive(Component)]
 pub struct FakeTransform {
     pub translation: Vec3,
+    pub scale: Option<Vec2>
 }
 
 impl FakeTransform {
     pub fn from_xyz(x: f32, y: f32, z: f32) -> Self {
-        Self { translation: Vec3::new(x, y, z) }
+        Self { translation: Vec3::new(x, y, z), scale: None }
+    }
+
+    pub fn from_xyz_and_scale(x: f32, y: f32, z: f32, scale_x: f32, scale_y: f32) -> Self {
+        Self { translation: Vec3::new(x, y, z), scale: Some(Vec2::new(scale_x, scale_y)) }
     }
 }
 
@@ -19,6 +24,13 @@ pub fn update_positions(
     for (fake_pos, mut pos) in query.iter_mut() {
         pos.translation.x = fake_pos.translation.x.round();
         pos.translation.y = fake_pos.translation.y.round();
+
+        if fake_pos.scale.is_some() {
+            // Assume initial size is round and short (scale = 0.5 ignored even if size = 2)
+            let scale = fake_pos.scale.unwrap();
+            pos.scale.x = scale.x.round();
+            pos.scale.y = scale.y.round();
+        }
     }
 }
 
